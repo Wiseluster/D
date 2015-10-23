acdc3faf771ba5715604cbc0f6abebb405f2cb36
Wiseluster <Wiseluster@users.noreply.github.com>
2015-10-24 07:00:13 +0800

    Update sort.d

--- sort.d
+++ sort.d
@@ -1,1 +1,16 @@
+import std.stdio,
+       std.random;
 
+void main()
+{
+	size_t len;
+	int[] arr;
+	
+	readf(" %s", &len);
+	arr.length = len;
+	foreach (ref el; arr)
+		el = uniform(10, 100);
+	
+	writeln(arr);
+	writeln(arr.sort);
+}
